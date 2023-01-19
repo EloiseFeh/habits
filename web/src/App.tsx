4a23760273a54ec91b4bb977cd9a4bdb6568996c
EloiseFEh <eloiseferreira045@gmail.com>
2023-01-18 22:36:05 -0300

    Setup Web

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import  './styles/global.css'
+
+import { Habit } from './components/Habit'
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={2}/>
+    </div>
+  )
+}
+
+export default App
