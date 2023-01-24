22cd9024ae5531a80eb9eaf875e7bed4d82018e3
EloiseFEh <eloiseferreira045@gmail.com>
2023-01-24 11:18:04 -0300

    Create Progress bar

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,24 @@
+interface ProfressBarProps {
+    progress: number
+}
+
+export function ProgressBar(props: ProfressBarProps){
+
+    const progressStyles = {
+        width: `${props.progress}%`
+    }
+
+    return(
+        <div className='h-3 rounded-xl bg-zinc-700 w-full nt-4'>
+                    <div
+                    role="progressbar"
+                    aria-label="Progresso de hpabitos completados nesse dia"
+                    aria-valuenow={props.progress}
+                    className='h-3 rounded-xl bg-violet-600'
+                    style={progressStyles}
+                    >
+
+                    </div>
+                </div>
+    )
+}
