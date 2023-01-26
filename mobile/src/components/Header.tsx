bfee005d2be417e8157ef97052403e897bf1bf32
EloiseFEh <eloiseferreira045@gmail.com>
2023-01-25 22:26:16 -0300

    refact components Header and Home to navigation

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,8 +2,12 @@ import { View, TouchableOpacity,Text } from "react-native";
 import Logo from '../assets/logo.svg';
 import {Feather} from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+import { useNavigation } from '@react-navigation/native';
 
 export function Header() {
+
+    const { navigate } = useNavigation();
+
     return(
         <View className="w-full flex-row items-center justify-between">
             <Logo />
@@ -11,6 +15,7 @@ export function Header() {
             <TouchableOpacity
             activeOpacity={0.7}
             className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+            onPress={() => navigate('new')}
             >
                 <Feather
                 name="plus"
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -2,14 +2,18 @@ import {View, Text, ScrollView} from "react-native";
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const weekDays = ['D', 'S', 'T','Q', 'Q', 'S', 'S'];
 
 const datesFromYearStart = generateRangeDatesFromYearStart();
 const minSummaryDatesSizes = 18 * 5;
 const amountOfDatesToFill = minSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home(){
+
+    const { navigate } = useNavigation();
+
     return(
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
